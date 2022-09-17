3aca117636253a6cd87e873b5c59f310b92d4d25
shrfrn <shrfrn@gmail.com>
2022-09-17 20:18:35 +0300

    Removed semicolons

--- js/main.js
+++ js/main.js
@@ -1,88 +1,88 @@
 'use strict'
 
 // Pieces Types
-const PAWN_BLACK = '♟';
-const ROOK_BLACK = '♜';
-const KNIGHT_BLACK = '♞';
-const BISHOP_BLACK = '♝';
-const QUEEN_BLACK = '♛';
-const KING_BLACK = '♚';
-const PAWN_WHITE = '♙';
-const ROOK_WHITE = '♖';
-const KNIGHT_WHITE = '♘';
-const BISHOP_WHITE = '♗';
-const QUEEN_WHITE = '♕';
-const KING_WHITE = '♔';
+const PAWN_BLACK = '♟'
+const ROOK_BLACK = '♜'
+const KNIGHT_BLACK = '♞'
+const BISHOP_BLACK = '♝'
+const QUEEN_BLACK = '♛'
+const KING_BLACK = '♚'
+const PAWN_WHITE = '♙'
+const ROOK_WHITE = '♖'
+const KNIGHT_WHITE = '♘'
+const BISHOP_WHITE = '♗'
+const QUEEN_WHITE = '♕'
+const KING_WHITE = '♔'
 
 // The Chess Board
-var gBoard;
-var gSelectedElCell = null;
+var gBoard
+var gSelectedElCell = null
 
 function restartGame() {
-    gBoard = buildBoard();
-    renderBoard(gBoard);
+    gBoard = buildBoard()
+    renderBoard(gBoard)
 }
 
 function buildBoard() {
-    var board;
+    var board
     // TODO: build the board 8 * 8
-    // console.table(board);
-    return board;
+    // console.table(board)
+    return board
 
 }
 
 function renderBoard(board) {
-    var strHtml = '';
+    var strHtml = ''
     for (var i = 0; i < board.length; i++) {
-        var row = board[i];
-        strHtml += '<tr>';
+        var row = board[i]
+        strHtml += '<tr>'
         for (var j = 0; j < row.length; j++) {
-            var cell = row[j];
+            var cell = row[j]
             // TODO: figure class name
-            var className = 'white';
-            var tdId = `cell-${i}-${j}`;
-            strHtml += `<td id="${tdId}" onclick="cellClicked(this)" class="${className}">${cell}</td>`;
+            var className = 'white'
+            var tdId = `cell-${i}-${j}`
+            strHtml += `<td id="${tdId}" onclick="cellClicked(this)" class="${className}">${cell}</td>`
         }
-        strHtml += '</tr>';
+        strHtml += '</tr>'
     }
-    var elMat = document.querySelector('.game-board');
-    elMat.innerHTML = strHtml;
+    var elMat = document.querySelector('.game-board')
+    elMat.innerHTML = strHtml
 }
 
 
 function cellClicked(elCell) {
 
     // TODO: if the target is marked - move the piece!
-    cleanBoard();
+    cleanBoard()
 
-    elCell.classList.add('selected');
-    gSelectedElCell = elCell;
+    elCell.classList.add('selected')
+    gSelectedElCell = elCell
 
-    // console.log('elCell.id: ', elCell.id);
-    var cellCoord = getCellCoord(elCell.id);
-    var piece = gBoard[cellCoord.i][cellCoord.j];
+    // console.log('elCell.id: ', elCell.id)
+    var cellCoord = getCellCoord(elCell.id)
+    var piece = gBoard[cellCoord.i][cellCoord.j]
 
-    var possibleCoords = [];
+    var possibleCoords = []
     switch (piece) {
         case ROOK_BLACK:
         case ROOK_WHITE:
-            possibleCoords = getAllPossibleCoordsRook(cellCoord);
-            break;
+            possibleCoords = getAllPossibleCoordsRook(cellCoord)
+            break
         case BISHOP_BLACK:
         case BISHOP_WHITE:
-            possibleCoords = getAllPossibleCoordsBishop(cellCoord);
-            break;
+            possibleCoords = getAllPossibleCoordsBishop(cellCoord)
+            break
         case KNIGHT_BLACK:
         case KNIGHT_WHITE:
-            possibleCoords = getAllPossibleCoordsKnight(cellCoord);
-            break;
+            possibleCoords = getAllPossibleCoordsKnight(cellCoord)
+            break
         case PAWN_BLACK:
         case PAWN_WHITE:
-            possibleCoords = getAllPossibleCoordsPawn(cellCoord, piece === PAWN_WHITE);
-            break;
+            possibleCoords = getAllPossibleCoordsPawn(cellCoord, piece === PAWN_WHITE)
+            break
 
     }
-    markCells(possibleCoords);
+    markCells(possibleCoords)
 }
 
 function movePiece(elFromCell, elToCell) {
@@ -96,17 +96,17 @@ function markCells(coords) {
 
 // Gets a string such as:  'cell-2-7' and returns {i:2, j:7}
 function getCellCoord(strCellId) {
-    var coord = {};
-    var parts = strCellId.split('-');
+    var coord = {}
+    var parts = strCellId.split('-')
     coord.i = +parts[1]
-    coord.j = +parts[2];
-    return coord;
+    coord.j = +parts[2]
+    return coord
 }
 
 function cleanBoard() {
-    var elTds = document.querySelectorAll('.mark, .selected');
+    var elTds = document.querySelectorAll('.mark, .selected')
     for (var i = 0; i < elTds.length; i++) {
-        elTds[i].classList.remove('mark', 'selected');
+        elTds[i].classList.remove('mark', 'selected')
     }
 }
 
@@ -119,25 +119,25 @@ function isEmptyCell(coord) {
 }
 
 function getAllPossibleCoordsPawn(pieceCoord, isWhite) {
-    var res = [];
+    var res = []
     // TODO: handle PAWN use isEmptyCell()
-    return res;
+    return res
 }
 
 function getAllPossibleCoordsRook(pieceCoord) {
-    var res = [];
+    var res = []
 
-    return res;
+    return res
 }
 
 function getAllPossibleCoordsBishop(pieceCoord) {
-    var res = [];
+    var res = []
 
-    return res;
+    return res
 }
 
 function getAllPossibleCoordsKnight(pieceCoord) {
-    var res = [];
+    var res = []
 
-    return res;
+    return res
 }
